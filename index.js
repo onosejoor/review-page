97f8bd68e13d3663d36887f38c218a1623f8ec01
Onos Ejoor <156698969+onosejoor@users.noreply.github.com>
2024-03-18 11:13:25 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-var rate = document.querySelector(".rating-card");
-var thank = document.querySelector(".thank-card");
-var submit = document.querySelector(".submit");
-var num = document.querySelector(".number").textContent;
-var list = document.querySelectorAll("li").length;
+let rate = document.querySelector(".rating-card");
+let thank = document.querySelector(".thank-card");
+let num = document.querySelector(".number").textContent;
+let list = document.querySelectorAll("li").length;
+let submit = document.querySelector(".submit");
 
 // for buttons (li's) //
 
-for (var i = 0; i < list; i++) {
-    document.querySelectorAll("li")[i].addEventListener("click", function (e) {
-let color = document.querySelector(".active");
-color.classList.remove("active");
-e.target.classList.add("active");
-num = e.target.textContent;
-        document.querySelector(".hidden").style.display = "none";
+for (let i = 0; i < list; i++) {
+    document.querySelectorAll(".lo")[i].addEventListener("click", function (e) {
+        for (let j = 0;  j< list; j++) {  
+        let  remove = document.querySelectorAll(".lo")[j]; 
+        remove.classList.remove("active");         
+        }
+        e.target.classList.add("active");
+        document.querySelector(".hidden").style.display = "none" 
+        var num2 =  e.target.textContent;
+        num = num2;
     });
-    
 }
 
-// for submit //
+// for submit buttons //
 
 submit.addEventListener("click", function () {
     if (num === "0") {
         document.querySelector(".hidden").style.display = "block";
-    }else{
-            rate.style.display = "none";
-            thank.style.display = "block";
-            document.querySelector(".number").textContent = num;
+    } else {
+    rate.style.display = "none";
+    thank.style.display = "block";
+    document.querySelector(".number").textContent = num;
     }
-
 });
